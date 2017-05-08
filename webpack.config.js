7a437ba2c8be91358e788c1ce31200b869ab7be9
steve2482 <stevecb12684@yahoo.com>
2017-05-07 21:37:53 -0400

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'js/index.js'),
+    entry: path.resolve(__dirname, 'public/js/index.js'),
     output: {
         path: path.resolve(__dirname, 'build/js'),
         filename: 'index.js',
