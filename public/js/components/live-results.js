e98877e8fe94f931b08cd35ebb8697f027e0ceb5
steve2482 <stevecb12684@yahoo.com>
2017-05-11 21:42:03 -0400

    server side request to youtube api for live broadcasts, need to update to get more than one result and scalable

--- public/js/components/live-results.js
+++ public/js/components/live-results.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Video from './video';
 
 export default function LiveResults() {
-  const videos = [<Video />, <Video />];
+  const videos = [<Video key='0' />, <Video key='1' />];
   return (
 		<div className='live-results box'>
       <h3>Current Live Broadcasts</h3>
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const fetch = require('isomorphic-fetch');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 app.use(express.static('public'));
 app.use(cors());
@@ -19,6 +24,25 @@ app.get('/channel-names', (req, res) => {
       channelNames.push(data[i].abreviatedName);
     }
     res.status(200).json(channelNames);
+  });
+});
+
+// get current live feeds
+app.get('/live', (req, res) => {
+  let apiKey = process.env.YOUTUBE_API_KEY;  
+  let channelId = 'UC76vyQZnIIF7iA5ta24ukVw';
+  fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&eventType=live&type=video&key=${apiKey}`)
+  .then(response => {
+    if (!response.ok) {
+      const error = new Error(response.statusText);
+      error.response = response;
+      throw error;
+    }
+    return response.json();
+  })
+  .then(response => res.json(response))
+  .catch(err => {
+    console.log('Error:', err);
   });
 });
 
