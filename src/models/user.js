f6c57b7cf7c778b87cb6e31d720b408d24a91579
steve2482 <stevecb12684@yahoo.com>
2017-05-22 12:22:37 -0400

    bcryptjs package, user schema methods

--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,37 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 // User Schema
-const userSchema = mongoose.Schema({
+const UserSchema = mongoose.Schema({
   name: String,
   userName: String,
   password: String,
   favoriteChannels: Array
 });
 
-const User = mongoose.model('Channel', userSchema);
+const User = module.exports = mongoose.model('User', UserSchema);
 
-module.exports = {User};
+module.exports.createUser = function(newUser, callback) {
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(newUser.password, salt, function(err, hash) {
+      newUser.password = hash;
+      newUser.save(callback);
+    });
+  });
+};
+
+module.exports.getUserByUsername = function(username, callback) {
+  let query = {userName: username};
+  User.findOne(query, callback);
+};
+
+module.exports.getUserById = function(id, callback) {
+  User.findById(id, callback);
+};
+
+module.exports.comparePassword = function(candidatePassword, hash, callback) {
+  bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    if (err) throw err;
+    callback(null, isMatch);
+  });
+};
