6e1d83558f1d00a3f51b7db5319699af8b0d4549
steve2482 <stevecb12684@yahoo.com>
2017-05-22 13:43:27 -0400

    email and verify password fields added

--- src/js/components/register.js
+++ src/js/components/register.js
@@ -11,8 +11,10 @@ export default class Register extends React.Component {
     e.preventDefault();
     const newUser = {
       name: this.refs.name.value,
+      email: this.refs.email.value,
       userName: this.refs.userName.value,
-      password: this.refs.password.value
+      password: this.refs.password.value,
+      password: this.refs.password2.value
     };
     this.props.dispatch(
       actions.registerUser(newUser)
@@ -24,8 +26,10 @@ export default class Register extends React.Component {
       <div className='register box'>
         <form onSubmit={this.registerUser}>
           <input type='text' ref='name' placeholder='Name' />
+          <input type='email' ref='email' placeholder='Email' />
           <input type='text' ref='userName' placeholder='UserName' />
           <input type='password' ref='password' placeholder='Password' />
+          <input type='password' ref='password2' placeholser='Re-type Password' />
           <input type='submit'/>
         </form>
       </div>
