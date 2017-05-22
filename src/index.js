20fa5e33025763f57879ebe0e881698b4c62c3c5
steve2482 <stevecb12684@yahoo.com>
2017-05-21 21:33:59 -0400

    file structure change fixed with react-create-app, environment variables fixed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import App from './js/components/app';
-
+import './css/index.css';
 import store from './store';
 
 ReactDOM.render(
