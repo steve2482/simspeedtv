b13d7ffc3ca10f3e7744612ff1466acba76e68be
steve2482 <stevecb12684@yahoo.com>
2017-06-02 15:51:05 -0400

    setting up component test files

--- src/js/components/channel.test.js
+++ src/js/components/channel.test.js
@@ -7,14 +7,33 @@ import {Link} from 'react-router';
 
 chai.use(chaiEnzyme());
 
-const Channel = ({ channel }) => <div>
+const Channel = ({ channel }) => {
+  // If Favorite Channels List
+  if (channel.favorites === undefined) {
+    return (
+      <div>
+        <strong>
+          <Link to={'/channels/' + channel.name} className='favoriteChannelName'>
+            {channel.name}
+          </Link>
+        </strong>
+      </div>
+    );
+  }
+  // If Channel List
+  else {
+    return (
+      <div>
         <strong>
           <Link to={'/channels/' + channel.name} className='channelName'>
             {channel.name}
           </Link>
         </strong>
         <p className='favorite-count'>({channel.favorites})</p>
-      </div>;
+      </div>
+    );
+  };
+};  
 
 describe('<Channel />', () => {
   let wrapper = null;
@@ -37,5 +56,29 @@ describe('<Channel />', () => {
   it('Displays the channel name and favorites', () => {
     expect(wrapper.find('.channelName').text()).to.equal('channel name');
     expect(wrapper.find('.favorite-count').text()).to.equal('(2)');
+  });
+});
+
+describe('<Channel />', () => {
+  let wrapper = null;
+  let props = null;
+
+  beforeEach(() => {
+    props = {
+      channel: {
+        name: 'channel name',
+        favorites: undefined
+      }
+    };
+    wrapper = shallow(<Channel {...props}/>);
+  });
+
+  it('Renders without crashing', () => {
+    shallow(<Channel {...props}/>);
+  });
+
+  it('Displays the channel name and favorites', () => {
+    expect(wrapper.find('.favoriteChannelName').text()).to.equal('channel name');
+    expect(wrapper).to.not.contain(<p className='favorite-count'></p>);
   });
 });
---
+++ src/js/components/sidebar.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import chai from 'chai';
+import {expect} from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import {mount, render, shallow} from 'enzyme';
+import {Link} from 'react-router';
+
+chai.use(chaiEnzyme());
