40c45b83c1ee7880c60891e9fa68e188289db6b0
steve2482 <stevecb12684@yahoo.com>
2017-06-02 12:41:35 -0400

    updated dev dependancies, channel component tests added

--- src/js/components/channel.test.js
+++ src/js/components/channel.test.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import chai from 'chai';
+import {expect} from 'chai';
 import chaiEnzyme from 'chai-enzyme';
 import {mount, render, shallow} from 'enzyme';
 import {Link} from 'react-router';
 
 chai.use(chaiEnzyme());
 
-import Channel from './channel';
+const Channel = ({ channel }) => <div>
+        <strong>
+          <Link to={'/channels/' + channel.name} className='channelName'>
+            {channel.name}
+          </Link>
+        </strong>
+        <p className='favorite-count'>({channel.favorites})</p>
+      </div>;
 
 describe('<Channel />', () => {
-  it('Renders without crashing', () => {
-    shallow(<Channel />);
+  let wrapper = null;
+  let props = null;
+
+  beforeEach(() => {
+    props = {
+      channel: {
+        name: 'channel name',
+        favorites: 2
+      }
+    };
+    wrapper = shallow(<Channel {...props}/>);
   });
 
-  it('Displays the channel name')
-  it('Displays the channel favorites')
+  it('Renders without crashing', () => {
+    shallow(<Channel {...props}/>);
+  });
+
+  it('Displays the channel name and favorites', () => {
+    expect(wrapper.find('.channelName').text()).to.equal('channel name');
+    expect(wrapper.find('.favorite-count').text()).to.equal('(2)');
+  });
 });
