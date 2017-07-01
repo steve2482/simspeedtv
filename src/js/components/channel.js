0107c7cd6d00e2ada74de9c273edc445e58cf8c5
steve2482 <stevecb12684@yahoo.com>
2017-06-30 21:38:29 -0400

    mobile menu closes on channel click

--- src/js/components/channel.js
+++ src/js/components/channel.js
@@ -1,30 +1,49 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import * as actions from '../actions/actions';
 
-export default function Channel(props) {
-  // If Favorite Channels List
-  if (props.favorites === undefined) {
-    return (
-      <div>
-        <strong>
-          <Link to={'/channels/' + props.name} className='channelName'>
-            {props.name}
-          </Link>
-        </strong>
-      </div>
-    );
+export class Channel extends React.Component {
+  constructor(props) {
+    super(props);
   }
-  // If Channel List
-  else {
-    return (
-      <div>
-        <strong>
-          <Link to={'/channels/' + props.name} className='channelName'>
-            {props.name}({props.favorites})
-          </Link>
-        </strong>
-        <p className='favorite-count'></p>
-      </div>
-    );
+
+  toggleMenu() {
+    this.props.dispatch(
+      actions.toggleSidebar());
+  }
+
+  render() {
+    // If Favorite Channels List
+    if (this.props.favorites === undefined) {
+      return (
+        <div>
+          <strong>
+            <Link to={'/channels/' + this.props.name} className='channelName' onClick={this.toggleMenu}>
+              {this.props.name}
+            </Link>
+          </strong>
+        </div>
+      );
+    }
+    // If Channel List
+    else {
+      return (
+        <div>
+          <strong>
+            <Link to={'/channels/' + this.props.name} className='channelName' onClick={this.toggleMenu}>
+              {this.props.name}({this.props.favorites})
+            </Link>
+          </strong>
+          <p className='favorite-count'></p>
+        </div>
+      );
+    }
   }  
 }
+
+const mapStateToProps = (state, props) => ({
+  state: state
+});
+
+export default connect(mapStateToProps)(Channel);
