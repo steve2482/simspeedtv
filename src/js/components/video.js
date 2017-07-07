b9c2d3bd72614faf66d6b9616471a63ff2294770
steve2482 <stevecb12684@yahoo.com>
2017-07-06 20:49:11 -0400

    fixed routing bug, sidebar margins adjusted

--- src/js/components/video.js
+++ src/js/components/video.js
@@ -6,7 +6,7 @@ export default function Video(props) {
   let video = props.info;
   let src = video.snippet.thumbnails.medium.url;
   return (
-  <Link id='video-info' to={'/' + video.id.videoId}>
+  <Link id='video-info' to={'/video/' + video.id.videoId}>
     <img id='video-thumbnail' width='320' height='180' src={src} />
     <h4 id='channel-title'>{video.snippet.channelTitle}</h4>
     <p id='video-title'>{video.snippet.title}</p>
