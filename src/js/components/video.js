c6569fcb1cdcba8f6182b4bdda5d3dd6385df8cb
steve2482 <stevecb12684@yahoo.com>
2017-07-10 22:11:13 -0400

    upcoming broadcast html, action, reducer, styles added

--- src/js/components/video.js
+++ src/js/components/video.js
@@ -3,13 +3,26 @@ import '../../css/video.css';
 import {Link} from 'react-router-dom';
 
 export default function Video(props) {
-  let video = props.info;
-  let src = video.snippet.thumbnails.medium.url;
-  return (
-  <Link id='video-info' to={'/video/' + video.id.videoId}>
-    <img id='video-thumbnail' width='320' height='180' src={src} />
-    <h4 id='channel-title'>{video.snippet.channelTitle}</h4>
-    <p id='video-title'>{video.snippet.title}</p>
-  </Link>
-  );
+  if (props.upcomingInfo) {
+    let video = props.upcomingInfo;
+    let src = video.thumbnail;
+    return (
+      <Link id='video-info' to={'/video/' + video.videoId}>
+        <img id='video-thumbnail' width='320' height='180' src={src} />
+        <h4 id='channel-title'>{video.channelTitle}</h4>
+        <p id='video-title'>{video.title}</p>
+        <p id='date'>{video.date}</p>
+      </Link>
+    );
+  } else {
+    let video = props.info;
+    let src = video.snippet.thumbnails.medium.url;
+    return (
+    <Link id='video-info' to={'/video/' + video.id.videoId}>
+      <img id='video-thumbnail' width='320' height='180' src={src} />
+      <h4 id='channel-title'>{video.snippet.channelTitle}</h4>
+      <p id='video-title'>{video.snippet.title}</p>
+    </Link>
+    );
+  }  
 }
