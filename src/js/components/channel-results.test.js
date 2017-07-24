393b46fd25c1dcb6d6a57b2d43ca7f8275949948
steve2482 <stevecb12684@yahoo.com>
2017-07-24 12:36:01 -0400

    updated mock state in tests

--- src/js/components/channel-results.test.js
+++ src/js/components/channel-results.test.js
@@ -18,6 +18,7 @@ describe('<ChannelResults />', () => {
     state: {
       channelNames: [],
       liveBroadcasts: [],
+      upcomingChannelBroadcasts: [],
       channelVideos: [],
       nextPageToken: null,
       user: '',
