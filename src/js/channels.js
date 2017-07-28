2e5feffc307c25fa94f1ff7f77ebcd17ff910e8b
steve2482 <stevecb12684@yahoo.com>
2017-07-28 09:44:49 -0400

    channel types broken out, list headers added, tests updated

--- src/js/channels.js
+++ src/js/channels.js
@@ -1,120 +1,154 @@
   {
-  abreviatedName: 'GSRC',
-  name: 'Global SimRacing Channel',
-  youtubeId: 'UC76vyQZnIIF7iA5ta24ukVw',
-  favorites: 0
+    abreviatedName: 'GSRC',
+    name: 'Global SimRacing Channel',
+    youtubeId: 'UC76vyQZnIIF7iA5ta24ukVw',
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'LSRTV',
     name: 'Live SimRacing TV',
     youtubeId: 'UCipmH2g_R0igpvmUkoYJfoA',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'RaceSpot',
     name: 'RaceSpotTV',
     youtubeId: 'UCyhYD5vb-N_0TJZMVegCVgw',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'ApexRacingTV',
     name: 'ApexRacing TV',
     youtubeId: 'UC3WV0kPwE7Llk7y7KyWpNug',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'GripTV',
     name: 'GripTV Live',
     youtubeId: 'UCP-e-TM5GpgVSjUhvSFpuZg',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'Digital Forge',
     name: 'Digital Forge Broadcasting Network',
     youtubeId: 'UC8WlmtLCOvUnrnfuwFltXrw',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'MaxSpeedTV',
     name: 'MaxSpeedTV',
     youtubeId: 'UCKe9FdVzYpvnajTYCNHComA',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'MaxSpeedTV2',
     name: 'MaxSpeedTV2',
     youtubeId: 'UCzNISbby9k0bB2SvKc3OrLQ',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'RaceAcademy',
     name: 'RaceAcademy Live TV',
     youtubeId: 'UCUXMbfPUOtTIZRtGdpULFvw',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'Prime Time Racing',
     name: 'Prime Time Racing TV',
     youtubeId: 'UCyTRI1iEhNeTTO6FDkeut3w',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'Non-StopRacing',
     name: 'Non-StopRacing TV',
     youtubeId: 'UCMQA-3PIHwr6NJKXpkx6BzQ',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'OSRN',
     name: 'Online SimRacing Network',
     youtubeId: 'UCI_mGEX1dumdMsy-ew-_FkA',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'Formula SimRacing',
     name: 'Formula SimRacing',
     youtubeId: 'UCb6FnVVxTQVm5QdxCxYMV2g',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'RRTV',
     name: 'RRTV',
     youtubeId: 'UCTcz_Na2BWwtp9pHd5cYDGA',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'v8sonline',
     name: 'V8sonilne',
     youtubeId: 'UCM6hpzaXiYP48MaUbAcvQkw',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'CFI',
     name: 'Checkerd Flag Interactive',
     youtubeId: 'UCppm6ASu9nj9EF8UP94ktGA',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'ASN',
     name: 'American Sim Network',
     youtubeId: 'UCfjr2WRM-6qZkDHb9wyk0Sw',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'CFR TV',
     name: 'Centerforce TV',
     youtubeId: 'UC9vTBFefgi9eocfD_ljuNdQ',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'Iracing',
     name: 'Iracing Official',
     youtubeId: 'UCPEcqkRG-kf2Vk6Rn_2WZSQ',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
   }
   {
     abreviatedName: 'APR',
     name: 'All Pro Broadcasting',
     youtubeId: 'UCd8brfaO1Rmf2sk9r7xhaHw',
-    favorites: 0
+    favorites: 0,
+    type: 'tv-style'
+  }
+  {
+    abreviatedName: 'ISRTV',
+    name: 'Inside SimRacing',
+    youtubeId: 'UCcpArg0eZs6FlhlCALfXFxA',
+    favorites: 0,
+    type : 'news'
+  }
+  {
+    abreviatedName: 'Simpit',
+    name: 'The Simpit',
+    youtubeId: 'UCqnakdO0DZ7w3ne7-EmaW7w',
+    favorites: 0,
+    type: 'news'
   }
